b750d18fcc59f8ac34ccc6508db1ad926287f3e9
Michelle Davis <67066019+MHargita@users.noreply.github.com>
2020-12-13 22:36:09 -0500

    d3 table select

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,23 @@
 // from data.js
 var tableData = data;
 
+
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+console.log(tableData);
+
+tableData.forEach(function(ufodata) {
+    console.log(ufodata);
+});
+
+tableData.forEach(function(ufodata) {
+    console.log(ufodata);
+    var row = tbody.append("tr");
+  
+    Object.entries(ufodata).forEach(function([key, value]) {
+      console.log(key, value);
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
